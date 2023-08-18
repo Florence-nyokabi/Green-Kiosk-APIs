4076bd35e6ba0864654b2f236241e6f92e12fff6
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-08-18 18:53:58 +0300

    Renamed Product Cart class

--- payment/views.py
+++ payment/views.py
@@ -8,21 +8,11 @@ from datetime import datetime
 def make_payment(request):
     product_cart = ProductCart.objects.all()
 
-    # total_price = sum(item.total_price() for item in product_cart)
+    
 
     if request.method == 'POST':
         form = PaymentForm(request.POST)
         if form.is_valid():
-            payment_details = form.cleaned_data  # Use cleaned_data instead of changed_data
-
-            payment = Payment.objects.create(
-                payment_method=payment_details['payment_method'],
-                # amount=total_price,
-                currency=payment_details['currency'],
-                date_of_payment=datetime.now(),
-                status='Pending'
-            )
-
             product_cart.delete()
             return redirect('payment_success')
     else:
